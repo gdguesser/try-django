a01886028452d1d654e35e13a5893effb06b53de
Gabriel Guesser <gabrieldguesser@hotmail.com>
2020-07-22 17:23:03 -0300

    creating some pages

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-	return HttpResponse("<h1>Hello World</h1>")
+def home_view(request, *args, **kwargs):
+	return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-	return HttpResponse("<h1>Hello Contacts</h1>")
+def contact_view(request, *args, **kwargs):
+	return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+	return render(request, "about.html", {})
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view, contact_view
+from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('contacts/', contact_view),
+    path('about/', about_view)
 ]
