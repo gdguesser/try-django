b25a31131edaa124894ad0afbbfb9b86af8e1676
Gabriel Guesser <gabrieldguesser@hotmail.com>
2020-07-26 23:07:42 -0300

    looping w django

--- src/pages/views.py
+++ src/pages/views.py
@@ -9,7 +9,12 @@ def contact_view(request, *args, **kwargs):
 	return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-	return render(request, "about.html", {})
+	my_context= {
+		"my_text": "this is about us",
+		"my_number": 996652279,
+		"my_list": [123, 321, "eae"]
+	}
+	return render(request, "about.html", my_context)
 
 def index(request):
 	return render(request, 'index.html', {})
